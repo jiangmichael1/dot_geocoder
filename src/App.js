bb93b7a7d4d99959addb53dc8a690eebe0470c71
jiangmichael1 <jiangm1@yahoo.com>
2023-03-28 22:38:10 -0400

    Testing out OpenLayer Map Library

--- src/App.js
+++ src/App.js
@@ -3,12 +3,30 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './containers/Header';
 import DataListing from './containers/DataListing';
 import DataDetail from './containers/DataDetail';
+import Map from 'ol/Map.js';
+import OSM from 'ol/source/OSM.js';
+import TileLayer from 'ol/layer/Tile.js';
+import View from 'ol/View.js';
+
+const map = new Map({
+  target: 'map',
+  layers: [
+    new TileLayer({
+      source: new OSM(),
+    }),
+  ],
+  view: new View({
+    center: [0, 0],
+    zoom: 2,
+  }),
+});
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Header/>
+        <Map></Map>
         <Routes>
           <Route path="/" exact element={<DataListing />} />
           <Route path="/data/:dataId" exact element={< DataDetail />} />
@@ -20,3 +38,5 @@ function App() {
 }
 
 export default App;
+
+
